9fc985a8e4b02e27d68fba563b1c85f1efc99a82
abeni-al7 <abenezeralebachew3@gmail.com>
2025-07-16 16:20:38 +0300

    Fix typo

--- models/task.go
+++ models/task.go
@@ -8,6 +8,6 @@ type Task struct {
 	Description string `json:"description"`
 	DueDate time.Time `json:"due_date"`
 	Status string `json:"status"`
-	CreatedAt time.Time `json:created_at`
-	UpdatedAt time.Time `json:updated_at`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
