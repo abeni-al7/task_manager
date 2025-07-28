0e7686caa20c1fa4796e233e8925a778765ec70f
abeni-al7 <abenezeralebachew3@gmail.com>
2025-07-28 10:39:33 +0300

    Mock the task repository

--- Usecases/mocks/mock_task_repo.go
+++ Usecases/mocks/mock_task_repo.go
@@ -1,7 +1,35 @@
 package mocks
 
-import "github.com/stretchr/testify/mock"
+import (
+	domain "github.com/abeni-al7/task_manager/Domain"
+	"github.com/stretchr/testify/mock"
+)
 
 type MockTaskRepo struct {
 	mock.Mock
+}
+
+func (m *MockTaskRepo) Create(task *domain.Task) (domain.Task, error) {
+	args := m.Called(task)
+	return args.Get(0).(domain.Task), args.Error(1)
+}
+
+func (m *MockTaskRepo) FetchAll() ([]domain.Task, error) {
+	args := m.Called()
+	return args.Get(0).([]domain.Task), args.Error(1)
+}
+
+func (m *MockTaskRepo) Fetch(idStr string) (domain.Task, error) {
+	args := m.Called(idStr)
+	return args.Get(0).(domain.Task), args.Error(1)
+}
+
+func (m *MockTaskRepo) Update(idStr string, task domain.Task) (domain.Task, error) {
+	args := m.Called(idStr, task)
+	return args.Get(0).(domain.Task), args.Error(1)
+}
+
+func (m *MockTaskRepo) Remove(idStr string) error {
+	args := m.Called(idStr)
+	return args.Error(0)
 }
